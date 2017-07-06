d1c87b061c02cb31ddc4777497a46a0b502d7e74
Jim Bogart <jimilives484@gmail.com>
2017-07-06 13:46:25 +0100

    Added rotation to tail scene

---
+++ NaturalEnvironment/Assets/Scripts/RotateScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+
+    public GameObject model;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    public void rightClick()
+    {
+
+
+        model.transform.Rotate(new Vector3(10, 10));
+    }
+	
+}
