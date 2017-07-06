07b8f315db43ade680a634142167a0c9d9d0b509
Jim Bogart <jimilives484@gmail.com>
2017-07-06 16:02:42 +0100

    Altered rotation script

--- NaturalEnvironment/Assets/Scripts/RotateScript.cs
+++ NaturalEnvironment/Assets/Scripts/RotateScript.cs
@@ -4,17 +4,26 @@ using System.Collections;
 public class RotateScript : MonoBehaviour {
 
     public GameObject model;
+    public float xRotation;
+    public float yRotation;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
-    public void rightClick()
+    public void tailHeavyRotate()
     {
 
 
-        model.transform.Rotate(new Vector3(10, 10));
+        model.transform.Rotate(new Vector3(xRotation, yRotation));
     }
-	
+
+    public void frontHeavyRotate()
+    {
+
+
+        model.transform.Rotate(new Vector3(-xRotation, yRotation));
+    }
+
 }
